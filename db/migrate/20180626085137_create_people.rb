4f2bf53928006f2a92843b139d77f69584d20e6d
Chase Jensen <soccerchase@gmail.com>
2018-06-26 02:58:39 -0600

    Person model, spec, migration, schema

---
+++ db/migrate/20180626085137_create_people.rb
@@ -0,0 +1,18 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :height
+      t.string :mass
+      t.string :hair_color
+      t.string :skin_color
+      t.string :eye_color
+      t.string :birth_year
+      t.string :gender
+      t.string :homeworld
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
